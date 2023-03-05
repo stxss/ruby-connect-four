4df2a00af6323822ec74b0a5632972d1f236f336
stxss <93729036+stxss@users.noreply.github.com>
2023-03-05 21:59:34 +0000

    Add colored slots functionality

---
+++ lib/slot.rb
@@ -0,0 +1,17 @@
+require_relative "./text_styles"
+
+class Slot
+    using TextStyles
+
+  def self.yellow
+    "\u{25cf}".fg_color(:yellow)
+  end
+
+  def self.blue
+    "\u{25cf}".fg_color(:light_blue)
+  end
+
+  def self.empty
+    "\u{25cb}"
+  end
+end

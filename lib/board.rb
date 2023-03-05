a4cbcf849e6f393f0f9791e877dbfa94c1da7f51
stxss <93729036+stxss@users.noreply.github.com>
2023-03-05 22:00:48 +0000

    Create method to create and show the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,16 @@
 require_relative("player")
+require_relative("slot")
 
 class Board
   attr_accessor :player1, :player2
 
   def initialize
+    @board = nil
+  end
+
+  def create_board
+    slot = Slot.empty
+    @board = Array.new(6) { Array.new(7, slot) }
   end
 
   def create_players
@@ -23,11 +30,45 @@ class Board
     end
   end
 
+  def show_board
+    if RUBY_PLATFORM =~ /win32/ || RUBY_PLATFORM =~ /mingw/
+        system("cls")
+    else
+        system("clear")
+    end
+
+    puts "\n" + scoreboard
+    @board.map { |row| puts "                #{row_join(row)}| \n #{splitter}"}
+    puts "\n"
+  end
+
+  private
+
+  def row_join(row)
+    row.map { |element| "| #{element} " }.join
+  end
+
+  def splitter
+    "               +" + "---+" * 7
+  end
+
+  def scoreboard
+    <<~HEREDOC
+        #{"+--------SCOREBOARD--------+".rjust(80)}
+        #{"|#{@player1.name.rjust(12)} - #{@player1.score.to_s}".rjust(69)}          |
+        #{"|#{@player2.name.rjust(12)} - #{@player2.score.to_s}".rjust(69)}          |
+        #{"+--------------------------+".rjust(80)}
+    HEREDOC
+  end
+
   def verify_name(prev_name, input)
     if /^[a-zA-Z]+$/.match?(input) && input != prev_name
-        input
+      input
     end
   end
 end
 
-# board = Board.new.create_players
+board1 = Board.new
+board1.create_players
+board1.create_board
+board1.show_board

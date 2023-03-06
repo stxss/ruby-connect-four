2440950b8fddcb699fd12f6275c8916a6a152587
stxss <93729036+stxss@users.noreply.github.com>
2023-03-06 20:29:05 +0000

    Delete lines that were transported into the game.rb file

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,4 @@
-require_relative("player")
 require_relative("slot")
-
 class Board
   attr_accessor :grid, :first_player, :second_player
 
@@ -27,15 +25,6 @@ class Board
     puts "\n"
   end
 
-  def ask_play
-    loop do
-      puts "Please, enter a valid position [1-7] to place your piece: "
-      input = gets.chomp.to_i
-      verified = verify_position(input)
-      place(verified) if verified
-    end
-  end
-
   private
 
   def row_join(row)
@@ -49,41 +38,9 @@ class Board
   def scoreboard
     <<~HEREDOC
       #{"+--------SCOREBOARD--------+".rjust(80)}
-      #{"|#{@player1.name.rjust(12)} - #{@player1.score}".rjust(69)}          |
-      #{"|#{@player2.name.rjust(12)} - #{@player2.score}".rjust(69)}          |
+      #{"|#{@first_player.name.rjust(12)} - #{@first_player.score}".rjust(69)}          |
+      #{"|#{@second_player.name.rjust(12)} - #{@second_player.score}".rjust(69)}          |
       #{"+--------------------------+".rjust(80)}
     HEREDOC
   end
-
-  def verify_name(prev_name, input)
-    input if /^[a-zA-Z]+$/.match?(input) && input != prev_name
-  end
-
-  def verify_position(input)
-    if input.between?(1, 7)
-      @turn += 1
-      place(input - 1)
-    end
-  end
-
-  def place(column)
-    @current_player_chip = @turn.odd? ? Slot.yellow : Slot.blue
-    5.downto(0) do |row|
-      if @board[row][column] == Slot.empty
-        @board[row][column] = @current_player_chip
-        show_board
-        break
-      elsif @board[0][column] != Slot.empty
-        break
-      else
-        next
-      end
-    end
-  end
 end
-
-board1 = Board.new
-board1.create_players
-board1.create_board
-board1.show_board
-board1.ask_play
